63e8546430f3364d93e15fdb803bb985c39a3a73
mouad <mr.robott52@gmail.com>
2025-05-05 12:20:44 +0100

    Div 2 deletion  commit

--- page scroll indicator/App.js
+++ page scroll indicator/App.js
@@ -1,11 +1,10 @@
-let indicator = document.querySelector(".scroll-indicator .progress");
-let scrollheight = document.documentElement.scrollHeight - document.documentElement.clientHeight ;
- 
-window.addEventListener("scroll", scroll);
+let indicator = document.querySelector('.scroll-indicator .progress');
+let scrollheight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-function scroll ( ){
+window.addEventListener('scroll',scroll);
+
+function scroll () {
   let scrollTop = document.documentElement.scrollTop ;
-  let scrolled = (scrollTop/scrollheight)*100;
+  let scrolled = (scrollTop/scrollheight)*100 ; 
   indicator.style.width = `${scrolled}%`
-  
-}
+};

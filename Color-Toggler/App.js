e5e3f3a79a856a6233eea78b34cab2b7ad14727b
mouad <mr.robott52@gmail.com>
2025-05-08 01:00:07 +0100

    Animated toggle commit

--- Color-Toggler/App.js
+++ Color-Toggler/App.js
@@ -1,10 +1,8 @@
-let toggler = document.getElementById('switch')
-
-toggler.addEventListener("click",()=>{
+let toggler = document.getElementById('switch');
+toggler.addEventListener('click',()=>{
   if (toggler.checked==true){
-    document.body.style.backgroundColor="black";
+    document.body.style.backgroundColor='black';
   }else{
-    document.body.style.backgroundColor="white";
-    
-  }
-});
+    document.body.style.backgroundColor='white';
+  } 
+})
